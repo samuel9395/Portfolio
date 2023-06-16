78cda3f4be06f1d318d47f824efb2e6da6320ac2
samuel9395 <88355342+samuel9395@users.noreply.github.com>
2023-06-16 00:11:19 -0300

    Add files via upload

---
+++ ProjetoCafe/Js/script.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-bnt').onclick = () =>{
+    navbar.classList.toggle('active');
+    searchform.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+
+let searchform = document.querySelector('.search-form');
+
+document.querySelector('#search-bnt').onclick = () =>{
+    searchform.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartitem.classList.remove('active');
+}
+
+let cartitem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-bnt').onclick = () =>{
+    cartitem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchform.classList.remove('active');
+    cartitem.classList.remove('active');
+} 
